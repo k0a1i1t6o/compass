bcddad7c6b1a9f20bb572eb2ef6fc79d542ceab6
k0a1i1t60 <kaikai25250116@gmail.com>
2018-10-25 15:24:58 +0900

    a
    b

--- js/app.js
+++ js/app.js
@@ -197,7 +197,7 @@
 
       // apply rotation to compass rose
       if (typeof rose.style.transform !== "undefined") {
-        rose.style.transform = "rotateZ(" + positionCurrent.hng + "deg)";
+        rose.style.transform = "rotateZ(" + 100 + "deg)";
       } else if (typeof rose.style.webkitTransform !== "undefined") {
         rose.style.webkitTransform = "rotateZ(" + positionCurrent.hng + "deg)";
       }
